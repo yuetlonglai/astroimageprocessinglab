5dd47ff25de5c6407ef041cc975f3a3d657398b1
yuetlonglai <lai.gordon2001@hotmail.com>
2023-03-05 23:12:35 +0000

    05/3/23 Update

--- astroimag_1.py
+++ astroimag_1.py
@@ -1,9 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import colors
 from scipy import optimize 
 import pandas as pd
 from astropy.io import fits
-from astropy.visualization import astropy_mpl_style, ImageNormalize, HistEqStretch, PercentileInterval, LinearStretch
+from astropy.visualization import astropy_mpl_style, ImageNormalize, HistEqStretch, PercentileInterval, AsymmetricPercentileInterval, LinearStretch
 
 # loading the data
 hdul = fits.open('A1_mosaic.fits')
@@ -27,14 +28,16 @@ popt,pcov = optimize.curve_fit(gaussian,midpoints,bin_heights,p0=[1e6,3421,10,0]
 plt.plot(midpoints,bin_heights,'x',color='cyan')
 pix = np.linspace(3.3e3,3.6e3,1000)
 plt.plot(pix,gaussian(pix,*popt),'-',color='salmon')
-plt.xlim(3.3e3,3.6e3)
+# plt.xlim(3.3e3,3.6e3)
 plt.show()
 
 # visualising the image
 # plt.style.use(astropy_mpl_style)
-plt.figure(figsize=(5,8))
-plt.imshow(imag,cmap='Greys',norm=ImageNormalize(imag,interval=PercentileInterval(0.1,16),stretch=LinearStretch(2,-1)))
-# plt.imshow(imag,cmap='Greys',norm=ImageNormalize(imag,interval=PercentileInterval(0.1,10),stretch=HistEqStretch(imag)))
-cbar = plt.colorbar()
+plt.figure(figsize=(12,8))
+plt.subplot(1,2,1)
+plt.imshow(imag,cmap='plasma',norm=colors.LogNorm())
+plt.subplot(1,2,2)
+plt.imshow(imag,cmap='Greys',norm=ImageNormalize(imag,interval=AsymmetricPercentileInterval(47,53,10),stretch=LinearStretch(5,-4)))
+plt.colorbar(label='Pixel Values')
 plt.show()
 
