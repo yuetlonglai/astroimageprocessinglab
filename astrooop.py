374e39659a9bd92f3dcd5a55f50a91c5d9c2019a
ljeaton <99902795+ljeaton@users.noreply.github.com>
2023-03-13 15:41:09 +0000

    Add files via upload

--- astrooop.py
+++ astrooop.py
@@ -32,12 +32,6 @@ class Process:
         hdul = fits.open(path)
         ZP, ZP_error = hdul[0].header['MAGZPT'], hdul[0].header['MAGZRR']
         return ZP, ZP_error
-    
-    def mask_edge(self, cut):
-        cutimg = self.img[cut:-cut, cut:-cut]
-        img2 = np.pad(cutimg, ((cut,cut),(cut,cut)), mode='constant', constant_values = (self.average_background))
-        # self.show_img()
-        return img2
 
     def circle_lowpass(self, cutoff, tau):
         self.show_img(self.img)
@@ -51,27 +45,26 @@ class Process:
         # print(imgfft)
 
         
-        imgmask = np.multiply(imgfft, circ) 
+        imgmask = np.multiply(imgfft, circ[:-1]) 
 
         self.show_img(circ)
         self.show_img(abs(imgmask))
         
 
         imgifft = fft.ifft2(imgmask)
-
-        # print(imgifft)
-
+        self.show_img(abs(imgifft))
         return abs(imgifft)
 
     def find_fit():
         pass
 
     def draw_circle(self, r, tau):
-        canv = np.full(self.img.shape, 0, dtype = float)
-        center = np.array(self.img.shape)/2.0
+        shape = np.asarray([int(i/2) for i in self.img.shape]) + [1,0]
+        canv = np.full(shape, 0, dtype = float)
+        center = np.array(self.img.shape, dtype = int)/2.0
         min_dist = 1e8 
-        for i, row in enumerate(canv):
-            for j, elem in enumerate(row):
+        for i in range((len(canv))):
+            for j in range((len(canv[i]))):
                 dist = (i- center[0])**2 + (j - center[1])**2
                 if  dist <= r ** 2:
                     canv[i,j] = 1
@@ -81,14 +74,8 @@ class Process:
                     if val < 0:
                         val = 0
                     canv[i,j] = val
-                    
-                    # if min_dist > dist - r**2:
-                    #     print(dist - r**2)
-                    #     print(val)
-                    #     min_dist = ((dist - r**2))
-
-                    #print(min_dist)
-
+        
+        canv = np.pad(canv,((0,shape[0]),(0,shape[1])), mode = 'reflect')
         return canv
     
     def clean_bleeding(self, bleedlist, clean_background = True):
@@ -109,10 +96,8 @@ class Process:
 
     def identify_objects(self):
         # apply threshold
-        # thresh = 3.5e3 # set threshold of the magnitude of objects
-        thresh = self.average_background + 2 * self.background_sigma
-        image_no_edge = self.mask_edge(125)
-        bw = morphology.closing(image_no_edge > thresh, morphology.disk(6))
+        thresh = 4.5e3 #set threshold of the magnitude of objects
+        bw = morphology.closing(self.img > thresh, morphology.disk(6))
         # remove artifacts connected to image border
         cleared = segmentation.clear_border(bw)
         # label image regions
@@ -126,7 +111,6 @@ class Process:
                 minr, minc, maxr, maxc = region.bbox
                 self.spotsx.append(int((minr+maxr)/2))
                 self.spotsy.append(int((minc+maxc)/2))
-        print('Number of Object Detected : ' + str(len(self.spotsx)))
         return self.spotsx, self.spotsy
     
     def aperture_photometry(self,radius=6):
@@ -147,7 +131,7 @@ class Process:
     def catalogue(self):
         y, x = self.identify_objects()
         z, zerr = self.aperture_photometry()
-        self.table = pd.DataFrame([],columns=['position-x','position-y','magnitude','magnitude-error'])
+        self.table = pd.DataFrame(columns=['position-x','position-y','magnitude','magnitude-error'], dtype = object)
         self.table['position-x'] = x
         self.table['position-y'] = y
         self.table['magnitude'] = z
@@ -179,36 +163,50 @@ class Process:
         ax[0].imshow(img,cmap='inferno', norm=colors.LogNorm())
         ax[1].imshow(img,cmap='Greys',norm=normalise)
         plt.show()
+
+
+    def mask_edge(self, cut):
+        cutimg = self.img[cut:-cut, cut:-cut]
+        self.img = np.pad(cutimg, ((cut,cut),(cut,cut)), mode='constant', constant_values = (self.average_background))
         
+        # self.show_img(np.where(cutimg not in self.img, self.average_background, self.img))
+
+    def set_img(self, newimg):
+        self.img = newimg
 # to do list: 
 # 1) improve object detection - right now we detect most of it but not all of it
 # 2) find the local background and use that to find flux instead of global background
 # 3) errorbars
-# 4) clean background
 
 
-# using the class
-image = Process('A1_mosaic.fits')
-# lp = img.circle_lowpass(700, 0.001)
+
+img = Process('A1_mosaic.fits')
+img.mask_edge(121)
+
+#img.circle_lowpass()
+
+
 bleeding = [(1400,1438),(514,558),(578,1456),(851,2133),(1292,776),(1196,2465),(1838,973),(2325,905),(2301,2131),(3184,2088),(4602,1431),(4488,1531)]
-image.clean_bleeding(bleeding,clean_background=False)
-# image.show_img()
-y,x = image.identify_objects()
-fluxlist = image.aperture_photometry()
+img.clean_bleeding(bleeding,clean_background=False)
+
+lp = img.circle_lowpass(1200, 0.01)
+lp2 = img.circle_lowpass(6,1)
+
+img.set_img(lp - lp2)
+
+img.show_img(lp - lp2)
+
+# img.show_img()
+y,x = img.identify_objects()
+fluxlist = img.aperture_photometry()
 # print(fluxlist)
 plt.figure(figsize=(10,8))
-plt.subplot(1,2,1)
-plt.imshow(image.img,cmap='inferno',norm=colors.LogNorm())
-plt.plot(x,y,'x',color='yellow',alpha=0.5)
-plt.subplot(1,2,2)
-normalise = visualization.ImageNormalize(image.img,interval=visualization.AsymmetricPercentileInterval(49,53,11),stretch=visualization.LinearStretch(2,-1))
-plt.imshow(image.img,cmap='Greys',norm=normalise)
-plt.plot(x,y,'x',color='yellow',alpha=0.5)
+plt.imshow(img.img,cmap='inferno',norm=colors.LogNorm())
+plt.plot(x,y,'x',color='yellow',alpha=0.2)
 plt.show()
 
-# Number of Count Plot
-counting = image.number_count()
-# print(counting[2],counting[3])
+counting = img.number_count()
+print(counting[2],counting[3])
 fit,cov = np.polyfit(counting[0][1:],np.log10(counting[1][1:]),1,cov=True)
 logN = np.poly1d(fit)
 print('Gradient = %.3e +/- %.3e' %(fit[0],np.sqrt(cov[0][0])))
@@ -218,6 +216,9 @@ plt.xlabel(r'$m$')
 plt.ylabel(r'$log(N(m))$')
 plt.errorbar(x=counting[0],y=np.log10(counting[1]),yerr=(np.log10(counting[2]),np.log10(counting[3])),fmt='.',capsize=2,color='blue',label='Data')
 plt.plot(counting[0],logN(counting[0]),'-',color='red',label='Fitted')
+# plt.plot(counting[0],expected-min(expected)+min(np.log10(counting[1])),'--',color='black')
+# plt.plot(counting[0],expected-max(expected)+max(np.log10(counting[1])),'--',color='black')
+# plt.fill_between(counting[0],expected-max(expected)+max(np.log10(counting[1])),expected-min(expected)+min(np.log10(counting[1])),color='grey',alpha=0.5)
 plt.legend(loc='best')
 plt.show()
         
