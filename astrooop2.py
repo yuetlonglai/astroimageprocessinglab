edd1720640e6de1e77036c72e452a43c1ac34613
yuetlonglai <lai.gordon2001@hotmail.com>
2023-03-20 10:57:25 +0000

    Code 20/3/23

--- astrooop2.py
+++ astrooop2.py
@@ -41,7 +41,7 @@ class Process2:
         # self.show_img()
         return img2
     
-    def clean_bleeding(self, bleedlist, clean_background = False):
+    def clean_bleeding(self, bleedlist):
         # remove stars that bleeds through the image -> irrelevant to analysis so remove
         # need a list of the coordinates of the stars that bleed in the image
         for i in bleedlist: 
@@ -52,58 +52,78 @@ class Process2:
                 connectivity = 5,
                 tolerance = self.img[i[0]][i[1]]-self.average_background-0.05e3
                 )
-        # clean background? (optional) (pick False if don't wanna clean)
-        if clean_background == True:
-            self.img = segmentation.flood_fill(self.img,bleedlist[0],self.average_background,connectivity=5,tolerance=1e3)
-            # self.img = np.where(self.img < 100, 0, self.img)
         return self
     
-    def identify_objects(self,catalogue=False):
+    def identify_objects(self,catalogue=False,aperture_vary=True):
         # remove background
         self.bkg = background.Background2D(self.img,(50,50),filter_size=(3,3),sigma_clip=stats.SigmaClip(sigma=3.0),bkg_estimator=background.MedianBackground())
         self.img = self.img - self.bkg.background
+        # mask
+        mask = np.ones(self.img.shape, dtype=bool)
+        mask[150:-150, 150:-150] = False
         # finding sources
         daofind = detection.DAOStarFinder(fwhm=7.0,threshold=5*self.background_sigma)
-        sources = daofind(self.mask_edge(150))
-        print('Number of Object Detected : ' + str(len(sources)))
+        sources = daofind(self.img, mask=mask)
         sourcesx = sources['xcentroid']
         sourcesy = sources['ycentroid']
-        # aperture photometry
+        sources_radius = 5.0 * np.log10(sources['peak']) - 5.0 # model the relationship between brightness and its radius approx => works
         positions = np.column_stack([sourcesx,sourcesy])
-        apertures = [aperture.CircularAperture(positions, r=r) for r in [6.0,8.0,10.0]]
-        phot_table = aperture.aperture_photometry(self.img, apertures)
+        # aperture photometry
+        if aperture_vary == False:
+            # with fixed aperture(s)
+            multi_apertures = [aperture.CircularAperture(positions, r=r) for r in [6.0,8.0,10.0]]
+            self.apertures = aperture.CircularAperture(positions, r=6.0)
+            phot_table = aperture.aperture_photometry(self.img, multi_apertures, method='subpixel')
+        else:
+            # with varying apertures
+            varying_phot = []
+            self.apertures = []
+            for i in range(len(positions)):
+                individual_apertures = aperture.CircularAperture(positions[i],r=sources_radius[i])
+                photo = aperture.aperture_photometry(self.img,individual_apertures)
+                varying_phot.append(photo['aperture_sum'])
+                self.apertures.append(individual_apertures)
+        # return method
         if catalogue == False:
+            print('Number of Object Detected : ' + str(len(sources)))
             return sourcesy, sourcesx
         else:
             catalogue_table = pd.DataFrame(columns=['x-centroid','y-centroid'])
             catalogue_table['x-centroid'] = sourcesx
             catalogue_table['y-centroid'] = sourcesy
-            catalogue_table['magnitude_0'] = self.zpinst - 2.5*np.log10(phot_table['aperture_sum_0'])
-            catalogue_table['magnitude_1'] = self.zpinst - 2.5*np.log10(phot_table['aperture_sum_1'])
-            catalogue_table['magnitdue_2'] = self.zpinst - 2.5*np.log10(phot_table['aperture_sum_2'])
+            if aperture_vary == False:
+                catalogue_table['magnitude_0'] = self.zpinst - 2.5*np.log10(phot_table['aperture_sum_0'])
+                catalogue_table['magnitude_1'] = self.zpinst - 2.5*np.log10(phot_table['aperture_sum_1'])
+                catalogue_table['magnitude_2'] = self.zpinst - 2.5*np.log10(phot_table['aperture_sum_2'])
+            else:
+                catalogue_table['magnitude_'] = self.zpinst - 2.5*np.log10(np.array(varying_phot))
             return catalogue_table
         
-    def number_count(self, plotting=False):
+    def number_count(self, plotting=False,num = 0):
         cat = self.identify_objects(catalogue=True)
-        magnitude_radius = cat['magnitude_0']
-        m = np.linspace(min(magnitude_radius),18,50)
+        aperture_num = num
+        magnitude_radius = cat['magnitude_'+str(aperture_num)]
+        m = np.linspace(9.8,18,50)
         N = []
         Nerr= []
         for i in m:
             N.append(np.count_nonzero(np.where(magnitude_radius < i, True, False)))
             # Nerr_l.append(np.count_nonzero(np.where(cat['magnitude']-abs(cat['magnitude-error']) < i, True, False)))
             # Nerr_u.append(np.count_nonzero(np.where(cat['magnitude']+abs(cat['magnitude-error']) < i, True, False)))
+        N = np.array(N)
+        m, N = m[1:], N[1:]
+        logN = np.log10(N)
+        logNerr = (1/N * N**0.5)*logN
         if plotting == True:
             print(N)
-            fit,cov = np.polyfit(m[1:],np.log10(N[1:]),1,cov=True)
-            logN = np.poly1d(fit)
+            fit,cov = np.polyfit(m,logN,1,w=1/logNerr,cov=True)
+            plogN = np.poly1d(fit)
             print('Gradient = %.3e +/- %.3e' %(fit[0],np.sqrt(cov[0][0])))
             plt.figure()
             plt.xlabel(r'$m$')
             plt.ylabel(r'$log(N(m))$')
-            # plt.errorbar(x=counting[0],y=np.log10(counting[1]),yerr=(np.log10(counting[2]),np.log10(counting[3])),fmt='.',capsize=2,color='blue',label='Data')
-            plt.errorbar(x=m[1:],y=np.log10(N[1:]),fmt='.',color='blue',label='Data')
-            plt.plot(m[1:],logN(m[1:]),'-',color='red',label='Fitted')
+            plt.errorbar(x=m,y=logN,fmt='.',yerr=logNerr,capsize=2,label='Data '+str(aperture_num),color='blue')
+            plt.plot(m,plogN(m),'-',color='red',label='Fitted')
             plt.legend(loc='best')
             plt.show()
         return m, N#, abs(np.array(N)-np.array(Nerr_l)), abs(np.array(Nerr_u)-np.array(N))
@@ -111,16 +131,21 @@ class Process2:
 
 # using the class
 image = Process2('A1_mosaic.fits')
-# image.double_lowpass()
-bleeding = [(1400,1438),(514,558),(578,1456),(851,2133),(1292,776),(1196,2465),(1838,973),(2325,905),(2301,2131),(3184,2088),(4602,1431),(4488,1531)]
-image.clean_bleeding(bleeding,clean_background=False)
+# clean bleeding
+bleeding = [(1400,1438),(514,558),(578,1456),(851,2133),(1292,776),(1196,2465),(1838,973),(2325,905),(2301,2131),(3184,2088),(4602,1431),(4488,1531),(4183,1036),(4275,1642)]
+image.clean_bleeding(bleeding)
+# identify objects
 y,x = image.identify_objects()
+# plot
 plt.figure(figsize=(10,8))
 normalise = visualization.ImageNormalize(image.img,interval=visualization.AsymmetricPercentileInterval(5,95),stretch=visualization.LinearStretch())
 plt.imshow(image.img,cmap='Greys',norm=normalise)
-# plt.imshow(image.img,cmap='inferno',norm=colors.LogNorm())
+# plt.imshow(image.original,cmap='inferno',norm=colors.LogNorm())
 plt.plot(x,y,'.',color='red',alpha=0.5)
+for i in range(len(image.apertures)):
+    image.apertures[i].plot(color='blue', lw=1.5, alpha=0.5)
 plt.colorbar()
 plt.show()
 
-image.number_count(plotting=True)
+# cumulative number count plot
+image.number_count(plotting=True,num='')
