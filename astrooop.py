72ab1e1f3a0e11ddce8e5d90ad0288513a63ee42
ljeaton <lje20@ic.ac.uk>
2023-03-23 10:49:07 +0000

    mltraining

--- astrooop.py
+++ astrooop.py
@@ -48,7 +48,7 @@ class Process:
         # self.show_img()
         return img2
 
-    def circle_lowpass(self, cutoff, tau):
+    def circle_lowpass(self, cutoff, tau, hp = False):
         # self.show_img(self.img)
         circ = self.draw_circle(cutoff, tau)
 
@@ -57,17 +57,21 @@ class Process:
         # self.show_img(abs(imgfft))
         # print(imgfft)
 
-        imgmask = np.multiply(imgfft, circ[:len(imgfft[0]),:len(imgfft[1])]) 
+        imgmask = np.multiply(imgfft, circ[:imgfft.shape[0],:imgfft.shape[1]]) 
 
         # self.show_img(circ)
         # self.show_img(abs(imgmask))
 
         imgifft = fft.ifft2(imgmask)
-        # self.show_img(abs(imgifft))
+        if hp:
+            hp_mask = np.multiply(imgfft,1 - circ[:imgfft.shape[0],:imgfft.shape[1]])
+            imghp = fft.ifft2(hp_mask)
+
+            return (abs(imgifft), abs(imghp))
+
+
         return abs(imgifft)
 
-    def find_fit():
-        pass
 
     def draw_circle(self, r, tau):
         shape = np.asarray([int(i/2) for i in self.img.shape]) + [1,0]
@@ -202,7 +206,7 @@ class Process:
         ax[1].imshow(img,cmap='Greys',norm=normalise)
         plt.show()
 
-    def double_lowpass(self):
+    def double_lowpass(self, hifrq, lowfreq):
         lp = self.circle_lowpass(1600, 0.01)
         
         lp2 = self.circle_lowpass(3,1)
@@ -212,24 +216,35 @@ class Process:
 
 class Process_fake(Process):
     def __init__(
-        cls, size = [100,100], noise_params = {'octaves': 5, 'seed': 1, 'scale': 10, 'shift': 10}, 
-        objparams = {'numgal': 10, 'numstar': 10, 'galamp': 4000, 'staramp': 4000, 'galwidth': 20, 'starwidth': 10}) -> None:
+        cls, size = [100,100], noise_params = {'octaves': 5, 'seed': 1, 'scale': 10, 'shift': 10, 'whiterange': 36, 'whiteshift': 5}, 
+        objparams = {'numgal': 10, 'numstar': 10, 'galamp': 4000, 'staramp': 4000, 'galwidth': 20, 'gnrange': [3,7], 'starwidth': 10}) -> None:
 
 
-        noisegrid = np.random.rand(noise_params['octaves'],noise_params['octaves'])*noise_params['scale'] + noise_params['shift']
-        nx = np.linspace(0,noise_params['octaves'], noise_params['octaves'])
-        ny = np.linspace(0,noise_params['octaves'], noise_params['octaves'])
+        noise = PerlinNoise(octaves = noise_params['octaves'], seed = noise_params['seed'])
+        noisegrid = np.zeros((100,100))
+        for i in tqdm(range(100)):
+            for j in range(100):
+                noisegrid[i,j] = (noise([i/100, j/100]))*noise_params['scale'] + noise_params['shift']
+
+        #noisegrid = np.random.rand(noise_params['octaves'],noise_params['octaves'])*noise_params['scale'] + noise_params['shift']
+        nx = np.linspace(0,100, 100)
+        ny = np.linspace(0,100, 100)
         ninter = RegularGridInterpolator((nx,ny),noisegrid)
-        x = np.linspace(0,noise_params['octaves'],size[0])
-        y = np.linspace(0,noise_params['octaves'],size[1])
+        x = np.linspace(0,100,size[0])
+        y = np.linspace(0,100,size[1])
 
         X, Y = np.meshgrid(x, y, indexing='ij')
-        print()
-        pic = ninter((X,Y))
 
-        # pickles = glob('*backpickle*')
-        # print(pickles)
-        # check = [str(noise_params['seed']) not in (i) or (str(size[0]) + '_' + str(size[1])) not in (i) for i in pickles]
+
+
+        pic = ninter((X,Y))
+        wnoise = np.random.rand(size[0],size[1])*noise_params['whiterange'] + noise_params['whiteshift']
+        pic += wnoise
+        
+
+        #pickles = glob('*backpickle*')
+        #print(pickles)
+        #check = [str(noise_params['seed']) not in (i) or (str(size[0]) + '_' + str(size[1])) not in (i) for i in pickles]
         # print(check)
         # if all(check):
         #     noise = PerlinNoise(octaves = noise_params['octaves'], seed = noise_params['seed'])
@@ -244,76 +259,81 @@ class Process_fake(Process):
         #     with open('{}_{}_backpickle_{}.pkl'.format(size[0],size[1],noise_params['seed']), 'rb') as f:
         #         pic = pickle.load(f)
             
-
+        
         
         cls.img = pic
         print(cls.img)
         print(cls.img.shape)
         cls.show_img()
-        cls.rand_galaxy(objparams['numgal'],objparams['galamp'],objparams['galwidth'], objparams['galskew'])
-        cls.rand_star(objparams['numstar'],objparams['staramp'],objparams['starwidth'])
+        cls.galprops =  cls.rand_galaxy(objparams['numgal'],objparams['galamp'],objparams['galwidth'], objparams['galskew'], objparams['gnrange'])
+        cls.starprops = cls.rand_star(objparams['numstar'],objparams['staramp'],objparams['starwidth'])
         cls.average_background = noise_params['shift']
         cls.background_sigma = noise_params['scale']/2
         cls.show_img()
 
 
-    def rand_galaxy(self, num, maxamp, maxwidth, skew):
+    def rand_galaxy(self, num, ampr, widthr, skew, nrange):
         xpoints = np.random.randint(len(self.img[0]),size = num)
         ypoints = np.random.randint(len(self.img[:,0]),size = num)
 
-        xwidths = np.random.randint(1,maxwidth,size = num)
-        ywidths = np.random.uniform(1/skew,1, size = num) * xwidths
-
-        amps = np.random.randint(1,maxamp, size = num)
+        xrs = np.random.randint(min(widthr),max(widthr),size = num)
+        skews = abs(np.random.rand(num))*abs(skew[0]-skew[1]) + min(skew)
+        amps = np.random.randint(min(ampr),max(ampr), size = num)
 
         x = np.arange(0,len(self.img[0]),1, dtype=int)
         y = np.arange(0,len(self.img[1]),1, dtype=int)
 
-        
+        ns = np.random.rand(num) * (max(nrange) - min(nrange)) + min(nrange)
 
         
-
-        for i, (x0,y0,xsig,ysig,a) in tqdm(enumerate(zip(xpoints,ypoints,xwidths,ywidths,amps))):
+        coords = []
+        swidths = []
+        for i, (x0,y0,xr,s,n,a) in tqdm(enumerate(zip(xpoints,ypoints,xrs,skews,ns,amps))):
+            
             angle = np.random.randint(0,360,1)
-            gaus = np.zeros((x.size,y.size), dtype=float)
 
-            maxsig = max([xsig,ysig])
-
-            xmask = (x < x0 + 4*maxsig) & (x > x0 - 4*maxsig)
-            ymask = (y0 - 4*maxsig < y) & (y < y0 + 4*maxsig)
+            xmask = (x < x0 + 4*xr) & (x > x0 - 4*xr)
+            ymask = (y0 - 4*xr < y) & (y < y0 + 4*xr)
 
             xm = x[xmask]
             ym = y[ymask]
 
-            if ym.size == xm.size: 
-                xx, yy = np.meshgrid(xm,ym)
-                gaus[xmask][:,ymask] += self.gaussian2d(xx,yy,x0,y0,xsig,ysig,a)
-                print(np.amax(gaus))
-            # print(x.size - gaus.shape[0])
-                # gaus = np.pad(gaus, (x.size - gaus.shape[0], y.size - gaus.shape[1]))
+            if xm.size == ym.size:
 
-                gimg = PIL.Image.fromarray(gaus)
-                gimg = gimg.rotate(angle, center = [x0,y0])
+                xx, yy = np.meshgrid(xm,ym)
+                sg = self.sersic(xx,yy,x0,y0,a,xr,n,s)
+                
+                sers = np.pad(sg, ([min(xm),x.size - min(xm) + x.size],[min(ym),y.size - min(ym)+ym.size] ))
+                sers = sers[:x.size,:y.size]
+
+                gimg = PIL.Image.fromarray(sers)
+                gimg = gimg.rotate(angle, center = [y0,x0])
+                #print(x0,y0,xr,s,n,a)
+                # plt.imshow(sers, norm=colors.LogNorm())
+                # plt.scatter(y0,x0, color = 'r',alpha=0.5)
+                # plt.show()
+
+                coords.append(np.array([x0,y0]))
+                swidths.append(np.array([xr,xr/s]))
+
                 self.img += np.array(gimg)
 
-        return {'coords': zip(xpoints,ypoints), 'widths': zip(xwidths,ywidths), 'amps': amps}
+        return {'coords': np.array(coords), 'widths': np.array(swidths), 'amps': amps}
 
     def rand_star(self, num, maxamp, maxwidth):
         xpoints = np.random.randint(self.img.shape[0],size = num)
         ypoints = np.random.randint(self.img.shape[0],size = num)
         
-        widths = np.random.randint(1,maxwidth,size = num)
+        widths = np.random.randint(min(maxwidth),max(maxwidth),size = num)
 
-        amps = np.random.randint(maxamp, size = num)
+        amps = np.random.randint(min(maxamp),max(maxamp), size = num)
 
         x = np.arange(0,len(self.img[0]),1, dtype=int)
         y = np.arange(0,len(self.img[1]),1, dtype=int)
 
-
+        coords = []
+        swidths = []
         for i, (x0,y0,sig,a) in tqdm(enumerate(zip(xpoints,ypoints,widths,amps))):
-
-            gaus = np.zeros((x.size,y.size), dtype=float)
-
 
             xmask = (x < x0 + 4*sig) & (x > x0 - 4*sig)
             ymask = (y0 - 4*sig < y) & (y < y0 + 4*sig)
@@ -321,45 +341,159 @@ class Process_fake(Process):
             xm = x[xmask]
             ym = y[ymask]
 
-            xx, yy = np.meshgrid(xm,ym)
-            gaus[xmask][:,ymask] += self.gaussian2d(xx,yy,x0,y0,sig,sig,a)
+            if ym.size == xm.size: 
 
-            self.img += gaus
+                xx, yy = np.meshgrid(xm,ym)
+                sg = self.gaussian2d(xx,yy,x0,y0,sig,sig,a)
+                gaus = np.pad(sg, ([min(xm),x.size - min(xm) + x.size],[min(ym),y.size - min(ym)+ym.size]))
+                gaus = gaus[:x.size,:y.size]
+                #gaus[xmask][:,ymask] += self.gaussian2d(xx,yy,x0,y0,sig,sig,a)
+                # plt.imshow(gaus, norm=colors.LogNorm())
+                # plt.scatter(y0,x0, color = 'r',alpha=0.5)
+                # plt.show()
+                self.img += gaus   
+                coords.append(np.array([x0,y0]))
+                swidths.append(np.array([sig,sig]))
 
-        return {'coords': zip(xpoints,ypoints), 'widths': zip(widths,widths), 'amps': amps}
+
+        return {'coords': np.array(coords), 'widths': np.array(swidths), 'amps': amps}
         
     def gaussian2d(self, x, y, x0, y0, sigma_x, sigma_y, amplitude):
         exponent = -((x-x0)**2/(2*sigma_x**2) + (y-y0)**2/(2*sigma_y**2))
         return amplitude * np.exp(exponent)
+
+    def sersic(self, x, y, x0, y0, Ie, Re, n, skew):
+        R = np.sqrt((x-x0)**2 + ((y - y0)/skew)**2)
+        I = (Ie)*np.exp((-2*n - 1/3) * ((R/Re)**(1/n)))
+        return I
     
+    def objwindow(self, x, y, coords, rng, wscale, wdim):
+        xmask = (coords[0] - wscale* rng < x) & (x < coords[0] +  wscale* rng)
+        ymask = (coords[1] - wscale * rng < y) & (y < coords[1] + wscale * rng)
+
+
+        window = self.img[xmask][:,ymask]
+
+        winterp = RegularGridInterpolator((x[xmask],y[ymask]),window)
+
+        nx = np.linspace(min(x[xmask]),max(x[xmask]),wdim[0])
+        ny = np.linspace(min(y[ymask]),max(y[ymask]),wdim[1])
+
+        NX, NY = np.meshgrid(nx,ny)
+
+        newdat = np.reshape(winterp((NX,NY)),(64,64,1))
+        return newdat
+
+
+
+
+    def createdataset(self, wdim = [64,64]):
+        x = np.linspace(0,self.img.shape[0],self.img.shape[0])
+        y = np.linspace(0,self.img.shape[1],self.img.shape[1])
+
         
-# to do list: 
-# 1) improve object detection - right now we detect most of it but not all of it
-# 2) find the local background and use that to find flux instead of global background
-# 3) errorbars
-
-testobjs = {'numgal': 100, 'numstar': 100, 'galamp': 1000, 'staramp': 2000, 'galwidth': 10, 'galskew': 2, 'starwidth': 5}
-test = Process_fake(size = [3000,3000], noise_params={'octaves': 3, 'seed': 2, 'scale': 17, 'shift': 3418.8155053212563}, objparams=testobjs)
-
-#using the class
-# image = Process('A1_mosaic.fits')
-
-# bleeding = [(1400,1438),(514,558),(578,1456),(851,2133),(1292,776),(1196,2465),(1838,973),(2325,905),(2301,2131),(3184,2088),(4602,1431),(4488,1531)]
-# image.clean_bleeding(bleeding,clean_background=False)
-
-image = test
-
-image.double_lowpass()
-y,x,by,bx = image.identify_objects(boxedge=True)
-plt.figure(figsize=(10,8))
-normalise = visualization.ImageNormalize(image.img,interval=visualization.AsymmetricPercentileInterval(5,95),stretch=visualization.LinearStretch())
-plt.imshow(image.img,cmap='Greys',norm=normalise)
-plt.plot(x,y,'.',color='red',alpha=0.5)
-for i in range(len(bx)):
-    plt.plot(bx[i],by[i],'-',color='blue',linewidth=1)
-plt.colorbar()
-plt.show()
-
-# #Number of Count Plot
-# image.number_count(plotting=True)
+        gwind = []
+        for i, (coord, sig) in enumerate(zip(self.galprops['coords'],self.galprops['widths'])) :
+            winsamp = self.objwindow(x,y,coord, np.amax(sig),2,wdim)
+            winsamp = (winsamp - np.amin(winsamp)) / np.amax(winsamp - np.amin(winsamp))
+            gwind.append(winsamp)
         
+        swind = []
+        for i, (coord, sig) in enumerate(zip(self.starprops['coords'],self.starprops['widths'])):
+            rsize = (np.random.random(1)*8 + 3)
+            winsamp = self.objwindow(x,y,coord, np.amax(sig),rsize,wdim)
+            winsamp = (winsamp - np.amin(winsamp)) / np.amax(winsamp - np.amin(winsamp))
+            swind.append(winsamp)
+        
+        gal = np.full(len(gwind),1)
+        star = np.full(len(swind),0)
+
+        allwinds = np.concatenate([gwind,swind])
+        alllabels = np.concatenate([gal,star])
+        print(alllabels)
+        return {'label': alllabels, 'data': allwinds}
+
+    def save_pkl(self, name):
+        with open(f'{name}.pkl', 'wb') as f:
+            pickle.dump(self,f)
+
+    def show_img(self, img = None, objscatter = False):
+        # plotting the image
+        if img is None:
+            img = self.img
+        normalise = visualization.ImageNormalize(
+            img,
+            interval=visualization.AsymmetricPercentileInterval(5,95),
+            stretch=visualization.LinearStretch()
+            )
+        fig, ax = plt.subplots(ncols=2,figsize=(10,8))
+        ax[0].imshow(img,cmap='inferno', norm=colors.LogNorm())
+        ax[1].imshow(img,cmap='Greys',norm=normalise)
+        if objscatter:
+            gcoords = self.galprops['coords']
+            scoords = self.starprops['coords']
+            ax[0].scatter(scoords[:,1],scoords[:,0],color = 'b', alpha = 0.5)
+            ax[0].scatter(gcoords[:,1],gcoords[:,0],color = 'r', alpha = 0.5)
+        plt.show()
+
+
+
+            
+
+            
+
+
+    
+if __name__ == '__main__':        
+    # to do list: 
+    # 1) improve object detection - right now we detect most of it but not all of it
+    # 2) find the local background and use that to find flux instead of global background
+    # 3) errorbars
+
+    # testobjs = {'numgal': 2000, 'numstar': 2000, 'galamp': [500,5000], 'staramp': [5000,500], 'galwidth': [20,2], 'galskew': [1,2], 'gnrange': [0.5,4] , 'starwidth': [10,2]}
+    # test = Process_fake(size = [3000,3000], noise_params={'octaves': 2, 'seed': 55, 'scale': 17, 'shift': 3418.8155053212563, 'whiterange': 5*36, 'whiteshift': 5}, objparams=testobjs)
+
+    # test.save_pkl('2000gal_2000star_3000_3000')
+
+    # test.show_img(objscatter = True)
+
+    with open('2000gal_2000star_3000_3000.pkl', 'rb') as f:
+        test = pickle.load(f)
+
+    test.show_img(objscatter = True)
+
+    data = test.createdataset()
+
+
+    for i,(d,lab) in enumerate(zip(data['data'],data['label'])):
+        print(lab)
+        test.show_img(d)
+        plt.show()
+
+    #using the class
+    #image = Process('A1_mosaic.fits')
+
+    #bleeding = [(1400,1438),(514,558),(578,1456),(851,2133),(1292,776),(1196,2465),(1838,973),(2325,905),(2301,2131),(3184,2088),(4602,1431),(4488,1531)]
+    #image.clean_bleeding(bleeding,clean_background=False)
+
+    #image = test
+
+    #lp, hp = image.circle_lowpass(2000, 0.5, True)
+
+    #image.show_img(hp)
+    #print(np.std(hp), np.mean(hp))
+    #image.double_lowpass()
+
+    # y,x,by,bx = image.identify_objects(boxedge=True)
+    # plt.figure(figsize=(10,8))
+    # normalise = visualization.ImageNormalize(image.img,interval=visualization.AsymmetricPercentileInterval(5,95),stretch=visualization.LinearStretch())
+    # plt.imshow(image.img,cmap='Greys',norm=normalise)
+    # plt.plot(x,y,'.',color='red',alpha=0.5)
+    # for i in range(len(bx)):
+    #     plt.plot(bx[i],by[i],'-',color='blue',linewidth=1)
+    # plt.colorbar()
+    # plt.show()
+
+    # #Number of Count Plot
+    # image.number_count(plotting=True)
+            
