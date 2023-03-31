96254c3ca9df3c07721b19ebfde79453ecdb806c
ljeaton <lje20@ic.ac.uk>
2023-03-31 19:32:12 +0100

    plots

--- astroimag_1.py
+++ astroimag_1.py
@@ -15,24 +15,24 @@ imag = hdul[0].data
 imag = imag[::-1]
 
 # visualising the statistics of the image
-imag1 = np.reshape(imag,len(imag)*len(imag[0]))
-# print(min(imag1),max(imag1))
-plt.figure()
-plt.xlabel('Pixel Values')
-plt.ylabel('Count')
-bin_heights, bin_edges, patches = plt.hist(imag1,bins=20000)
-midpoints = 0.5 * (bin_edges[1:]+bin_edges[:-1])
-def gaussian(x,a,b,c,d):
-    return a*np.exp(-(x-b)**2/c**2) +d
-popt,pcov = optimize.curve_fit(gaussian,midpoints,bin_heights,p0=[1e6,3421,10,0])
-plt.plot(midpoints,bin_heights,'x',color='cyan')
-pix = np.linspace(3.3e3,3.6e3,1000)
-plt.plot(pix,gaussian(pix,*popt),'-',color='salmon')
-# plt.xlim(3.3e3,3.6e3)
-plt.show()
-average_background, average_background_err = popt[1], np.sqrt(pcov[1][1])
-background_sigma = popt[2]
-print(average_background, average_background_err, background_sigma)
+# imag1 = np.reshape(imag,len(imag)*len(imag[0]))
+# # print(min(imag1),max(imag1))
+# plt.figure()
+# plt.xlabel('Pixel Values')
+# plt.ylabel('Count')
+# bin_heights, bin_edges, patches = plt.hist(imag1,bins=20000)
+# midpoints = 0.5 * (bin_edges[1:]+bin_edges[:-1])
+# def gaussian(x,a,b,c,d):
+#     return a*np.exp(-(x-b)**2/c**2) +d
+# popt,pcov = optimize.curve_fit(gaussian,midpoints,bin_heights,p0=[1e6,3421,10,0])
+# plt.plot(midpoints,bin_heights,'x',color='cyan')
+# pix = np.linspace(3.3e3,3.6e3,1000)
+# plt.plot(pix,gaussian(pix,*popt),'-',color='salmon')
+# # plt.xlim(3.3e3,3.6e3)
+# plt.show()
+# average_background, average_background_err = popt[1], np.sqrt(pcov[1][1])
+# background_sigma = popt[2]
+# print(average_background, average_background_err, background_sigma)
 
 # visualising the image in 3d
 # average_background = 3418.8155053212563 # from histogram gaussian fit
@@ -49,11 +49,11 @@ print(average_background, average_background_err, background_sigma)
 
 # visualising the image 
 # plt.style.use(astropy_mpl_style)
-plt.figure(figsize=(12,8))
-plt.subplot(1,2,1)
-plt.imshow(imag,cmap='plasma',norm=colors.LogNorm())
-plt.subplot(1,2,2)
-plt.imshow(imag,cmap='Greys',norm=ImageNormalize(imag,interval=AsymmetricPercentileInterval(49,53,8),stretch=LinearStretch(2,-1)))
-plt.colorbar(label='Pixel Values')
+plt.figure(figsize=(5,7))
+#plt.subplot(1,2,1)
+#plt.imshow(imag,cmap='inferno',norm=colors.LogNorm())
+#plt.subplot(1,2,2)
+plt.imshow(imag,cmap='Greys',norm=ImageNormalize(imag,interval=AsymmetricPercentileInterval(5,95),stretch=LinearStretch()))
+plt.tight_layout()
 plt.show()
 
